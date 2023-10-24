788528d1de13882de5899462276508eb8f11eeb0
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2023-10-24 15:47:25 -0700

    oh ... fix typo

--- gemstone/run.sh
+++ gemstone/run.sh
@@ -105,7 +105,7 @@ gemstone::prepare_gsdevkit_stones() {
 			local STONES_REGISTRY_NAME=smalltalkCI_run
 			createRegistry.solo $STONES_REGISTRY_NAME --ensure $GEMSTONE_DEBUG
 			createProjectSet.solo --registry=$STONES_REGISTRY_NAME --projectSet=$STONES_PROJECT_SET_NAME \
-				                 --from=$STONES_GSDEVKITSTONES_ROOT/projectSets/http/devKit.ston $GEMSTONE_DEBUG
+				                 --from=$STONES_GSDEVKITSTONES_ROOT/projectSets/http/devkit.ston $GEMSTONE_DEBUG
 			cloneProjectsFromProjectSet.solo  --registry=$STONES_REGISTRY_NAME --projectSet=$STONES_PROJECT_SET_NAME \
 				                 --projectDirectory=$STONES_PROJECTS_HOME $GEMSTONE_DEBUG
 			registerProductDirectory.solo --registry=$STONES_REGISTRY_NAME \
